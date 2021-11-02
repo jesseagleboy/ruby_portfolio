a9e1afeab492d5d2f749198045750d3517486d29
jesseagleboy <jslomowitz@gmail.com>
2021-11-02 11:50:44 -0400

    postgres working now

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_02_140421) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.text "imageLink"
     t.string "url"
