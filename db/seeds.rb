85e0f1f059d215e6a99a3a9bf90ba0cdffe93170
jesseagleboy <jslomowitz@gmail.com>
2021-11-02 14:33:44 -0400

    additional seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ Project.create(imageLink: 'https://repository-images.githubusercontent.com/41006
 Project.create(imageLink: 'https://repository-images.githubusercontent.com/399632979/1281b1a8-a127-408f-90ee-393d554e5da4', url: 'https://music-jamming.herokuapp.com/', alt: 'Jamming', cardTitle: 'Jamming', cardText: 'Use your Spotify account to create and edit a playlist with a search function. This is a Codecademy project.')
 
 Project.create(imageLink: 'https://repository-images.githubusercontent.com/376091559/515ae3ed-4359-4fff-8284-daff69c26de5', url: 'https://mixed-messages-j.herokuapp.com/', alt: 'Mixed Messages', cardTitle: 'Mixed Messages', cardText: 'Use three different APIs to generate random messages. This is a Codecademy project.')
+
+Project.create(imageLink: 'https://repository-images.githubusercontent.com/406119487/137931ad-c467-49fa-a54a-c813f3c94850', url: 'https://ravenous-search.herokuapp.com/', alt: 'Ravenous', cardTitle: 'Ravenous', cardText: 'Search for restaurants using the Yelp API. This is a Codecademy project.')
+
+Project.create(imageLink: 'https://repository-images.githubusercontent.com/284835104/87227a53-9960-4421-a9d7-c9594df3b4d2', url: 'https://jslomowitz-resume.herokuapp.com/', alt: '2020 Resume Design', cardTitle: '2020 Resume Design (Legacy)', cardText: 'This is my responsive design resume made in 2020. This is to show the progress of my skills from that resume to this portfolio.')
+
+
